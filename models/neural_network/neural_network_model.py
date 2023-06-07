021484389464df6fd37c84c273877743caeeb12c
Jacob Wong <jacobw5@uci.edu>
2023-06-06 21:57:08 -0700

    Implement stub derived classes for each classifier model

---
+++ models/neural_network/neural_network_model.py
@@ -0,0 +1,20 @@
+from utils.abstract_base_model import AbstractBaseModel
+
+class NeuralNetworkModel(AbstractBaseModel):
+    def initialize_model(self):
+        pass
+
+    def train_and_validate_model(self):
+        pass
+
+    def test_model(self, test_data):
+        pass
+
+    def evaluate_model(self, test_data_results):
+        pass
+
+    def print_results(self):
+        pass
+
+    def report_results(self):
+        pass
