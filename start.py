c6fc65b2a7b416928e37e0e4d949089dc447647c
Jacob Wong <jacobw5@uci.edu>
2023-06-08 03:20:20 -0700

    Add minor change

--- start.py
+++ start.py
@@ -13,7 +13,7 @@ def main():
     driver.load_preprocess_split_data()
         
     # Uncomment below lines when you're ready to implement the corresponding classifier
-    driver.create_knn_model(driver.training_data, driver.validation_data)
+    # driver.create_knn_model(driver.training_data, driver.validation_data)
     # driver.create_logistic_regression_model(driver.training_data, driver.validation_data)
     # driver.create_neural_network_model(driver.training_data, driver.validation_data)
 
